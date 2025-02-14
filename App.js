a08a76dc46bb2d487e5acb685c7a2a319c638e2e
Hajer Bouricha <hajer.bouricha@ensi-uma.tn>
2025-02-14 13:22:31 +0100

    React inception

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * <div id="parent">
+ *    <div id="child">
+ *        <h1> I am H1 tag</h1>
+ * <h2> I am H2 tag</h2>
+ *    </div>
+ *
+ * <div id="child2">
+ *        <h1> I am H1 tag</h1>
+ * <h2> I am H2 tag</h2>
+ *    </div>
+ * </div>
+ *createElement() =>html brower understand
+ *
+ *
+ *jsx
+ */
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, " I am H1 tag"),
+    React.createElement("h2", {}, " I am H2 tag"),
+    React.createElement("h3", {}, " I am H3 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, " I am H1 tag"),
+    React.createElement("h2", {}, " I am H2 tag"),
+    React.createElement("h3", {}, " I am H3 tag"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log(parent);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xzy: "abc" },
+//   "hello world from react"
+// );
+// console.log(heading);//return objet
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
