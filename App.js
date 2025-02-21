65ba086497f5980d8d12a380fc5e87ce96ca2493
Hajer Bouricha <hajer.bouricha@ensi-uma.tn>
2025-02-21 13:13:37 +0100

    Episode 3: Laying

--- App.js
+++ App.js
@@ -1,43 +1,21 @@
-/**
- * <div id="parent">
- *    <div id="child">
- *        <h1> I am H1 tag</h1>
- * <h2> I am H2 tag</h2>
- *    </div>
- *
- * <div id="child2">
- *        <h1> I am H1 tag</h1>
- * <h2> I am H2 tag</h2>
- *    </div>
- * </div>
- *createElement() =>html brower understand
- *
- *
- *jsx
- */
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, " I am H1 tag"),
-    React.createElement("h2", {}, " Learn parcel"),
-    React.createElement("h3", {}, " I am H3 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, " I am H1 tag"),
-    React.createElement("h2", {}, " I am H2 tag"),
-    React.createElement("h3", {}, " I am H3 tag"),
-  ]),
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log(parent);
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xzy: "abc" },
-//   "hello world from react"
-// );
-// console.log(heading);//return objet
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+// React Element
+const element = <span>React element</span>;
+const Title = () => <h1 className="head">I'm Hajer </h1>;
+// React functional component
+const number = 1000;
+const HeadingComponent = () => (
+  <div id="container">
+    <h2>{number}</h2>
+    {element}
+    <Title></Title>
+    <Title/>
+    {Title()}
+    <h1>Learn React</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
